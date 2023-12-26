b9f50b7b5d1c408c2769e5010fc6cf878ff68fa2
Merey <mereyazhibekova@gmail.com>
2023-12-26 22:54:28 +0600

    first commit

---
+++ .anaconda/navigator/scripts/notebook.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+. /Users/mereyazhibekova/anaconda3/bin/activate /Users/mereyazhibekova/anaconda3
+open /Users/mereyazhibekova/anaconda3/bin/jupyter_mac.command >/Users/mereyazhibekova/.anaconda/navigator/scripts/notebook-out-1.txt 2>/Users/mereyazhibekova/.anaconda/navigator/scripts/notebook-err-1.txt
